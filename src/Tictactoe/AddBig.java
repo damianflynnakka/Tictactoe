21cd59b1b6b84cc428c3f98d070104bd1db1512f
damo <damo@damof>
2018-10-23 16:40:38 +0100

    firstCommit

---
+++ src/Tictactoe/AddBig.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package friday;
+
+/**
+ *
+ * @author damo
+ */
+//public class AddBig {
+    
+ //   String pw = "";
+   // char character;
+ //   int total =0;
+  //  int mod;
+    
+  //  String Name = "dimitirius";
+
+  /*  public void setPw(String pw) {
+        this.pw = pw;
+    }*/
+    
+    
+    
+    
+        
+     /*   public String GenNum (){
+        
+        for (int i = 3; i< pw.length(); i++)
+        {
+            character = pw.charAt(i);
+            int ascii = (int) character;
+            total += ascii;
+            
+        } */
+        
+     //   mod = total % 7;
+        
+        //replace chain of if else with switch statement
+      /*  if (mod == 0 || mod == 1)
+        {
+            pw += 'X';
+        }
+        
+        else if  (mod == 2 || mod == 4)
+        {
+            pw += 'T';
+        }
+        
+        else if (mod == 5)
+        {
+            pw += 'A';
+        }
+        
+        else 
+        {
+            pw += 'C';
+        }
+        
+        
+        
+        return pw; 
+        
+        }
+    
+}
+*/
